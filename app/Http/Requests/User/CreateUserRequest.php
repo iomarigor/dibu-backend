4d082d8b633636ac40c43126646ad2ab537e3559
jordiValverde <55800542+JordiValverde@users.noreply.github.com>
2024-02-19 16:32:40 -0500

    Creación de servicios básicos Request para controladores principales

---
+++ app/Http/Requests/User/CreateUserRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Anik\Form\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    protected function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    protected function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
